3c82a485f046f58bcfc9f144b6b0dd01b587ac8e
MinhNhat127 <holemiinhnhatt@gmail.com>
2026-07-04 15:53:31 +0700

    Update graduation event time to 14:00 - 16:00

--- graduation-invitation/src/data/invitation.js
+++ graduation-invitation/src/data/invitation.js
@@ -1,7 +1,7 @@
 const invitation = {
   host: "Hồ Lê Minh Nhật (em min)",
   date: "Thứ 6, 10 tháng 7, 2026",
-  time: "17:00 – 19:00",
+  time: "14:00 - 16:00",
   location: "Hội trường Edison, Trường Đại học Đông Á",
   address: "33 Xô Viết Nghệ Tĩnh, Quận Hải Châu, Đà Nẵng",
   maps: "https://maps.app.goo.gl/kj95tocfqZfTKQej8",
