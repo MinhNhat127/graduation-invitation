06f96f3c50e9e8ee7163a0e4b16ae015b62d9e25
MinhNhat127 <holemiinhnhatt@gmail.com>
2026-07-04 15:07:23 +0700

    Update Google Maps navigation URL

--- graduation-invitation/src/data/invitation.js
+++ graduation-invitation/src/data/invitation.js
@@ -4,7 +4,7 @@ const invitation = {
   time: "17:00 – 19:00",
   location: "Hội trường Edison, Trường Đại học Đông Á",
   address: "33 Xô Viết Nghệ Tĩnh, Quận Hải Châu, Đà Nẵng",
-  maps: "https://maps.app.goo.gl/kj95tocfqZfTKQej8",
+  maps: "https://www.google.com/maps/place/Tr%C6%B0%E1%BB%9Dng+%C4%90%E1%BA%A1i+h%E1%BB%8Dc+%C4%90%C3%B4ng+%C3%81/@16.032034,108.2187276,724m/data=!3m2!1e3!4b1!4m6!3m5!1s0x314218389cf02c2b:0xbdc63233587e2d88!8m2!3d16.0320289!4d108.2213025!16s%2Fg%2F120ktg5z?entry=ttu&g_ep=EgoyMDI2MDYyOS4wIKXMDSoASAFQAw%3D%3D",
 };
 
 export default invitation;
